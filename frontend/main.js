5d366fac765c52be185f399bd55e737fc3a416b3
hamza14khan <hamzaubit14@gmail.com>
2024-05-17 11:17:39 +0100

    spike: wip header

--- frontend/main.js
+++ frontend/main.js
@@ -1,22 +1,5 @@
 import './style.scss'
 import { initAll } from '@ogcio/ogcio-ds'
-import logoHelper from './src/logo';
 
-const utils = {
-  insertHTMLByClass: (html, className) => {
-    var div = document.querySelector('.' + className);
-    if (div) {
-      div.innerHTML = html;
-    } else {
-      console.error('Div element with class ' + className + ' not found.');
-    }
-  }
-}
-
-function initApp() {
-  // Header
-  utils.insertHTMLByClass(logoHelper.getGovHeaderLogo(), 'govie-header__logotype')
-}
 
 initAll();
-initApp();
