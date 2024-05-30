1def80eb03839ce78b501bebfeb48a2015a86232
Hamza Khan <164040832+hamza14khan@users.noreply.github.com>
2024-05-30 11:38:06 +0100

    feat: add example dafm page (#3)

--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,5 @@
 import './style.scss'
-
 import { initAll } from '@ogcio/ogcio-ds'
 
+
 initAll();
-
-// Testing server here.
-async function fetchData() {
-  const response = await fetch('http://localhost:3000');
-  const data = await response.json();
-  document.getElementById('message').innerText = data.hello;
-}
-
-fetchData();
-
-document.querySelector('#app').innerHTML = `
-  <h3 style="padding: 50px">
-    Hello World
-  </h3>
-`
